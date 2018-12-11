ea21d7cea8ab481b790676789b27ddadd6742d95
MrBean1512 <39568733+MrBean1512@users.noreply.github.com>
2018-12-11 02:23:03 -0800

    Add files via upload

--- Stats.cpp
+++ Stats.cpp
@@ -15,4 +15,24 @@ void Stats::print_report()
 {
 	std::cout << "Total patients: " << num_patient << std::endl;
 	std::cout << "Average Total Service time: " << (double)num_total_time / num_patient << std::endl;
+	bool search;
+	std::cout << "Would you like to view the records for treated patients?\nEnter 0 if no or 1 if yes" << std::endl;
+	std::cin >> search;
+	if (search) {
+		std::map<int, Citizen*>::iterator it;
+		for (auto const& pair : records) {
+			std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+		}
+		int who=0;
+		while (who != -1) {
+			std::cout << "Enter the number of the person who's records you would like to view: ";
+			std::cin >> who;
+			it = records.find(who);
+			std::cout << records.find(who)->second->records[0];
+			std::cout << "Would you like to search another record?\nEnter -1 to exit and 0 to continue";
+			std::cin >> who;
+		}
+
+	}
+	return;
 }
