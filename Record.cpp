87ecd99a896c020170a3c677f81777a0c83343e7
MrBean1512 <39568733+MrBean1512@users.noreply.github.com>
2018-12-14 17:32:06 -0800

    Delete Record.cpp

--- Record.cpp
+++
@@ -1,8 +0,0 @@
-#include "Record.h"
-
-Record::Record(int sicknessLevel, int arrivalTime, int dismissalTime)
-{
-	this->sicknessLevel = sicknessLevel;
-	this->arrivalTime = arrivalTime;
-	this->dismissalTime = dismissalTime;
-}
